ada72b8fb81ce4215ef7c4609f68e3a6b916196c
aitopps <bikchaandroid@gmail.com>
2022-01-06 09:07:53 +0100

    create offer url

---
+++ src/routes/offers/[offer].svelte
@@ -0,0 +1,18 @@
+<script context="module">
+    export const load = ({ URL, params }) => {
+        console.log(params);
+        let offer = params.offer;
+
+        return {
+            props: {
+                offer,
+            },
+        };
+    };
+</script>
+
+<script>
+    export let offer;
+</script>
+
+<h1>{offer}</h1>
